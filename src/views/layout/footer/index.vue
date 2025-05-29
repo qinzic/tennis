c535027fc2098256cc5845563f32c53f4f3b2ef7
kongyinshui <2137198145@qq.com>
2025-05-29 22:47:05 +0800

    上传整个文件夹

---
+++ src/views/layout/footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.footer{
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
